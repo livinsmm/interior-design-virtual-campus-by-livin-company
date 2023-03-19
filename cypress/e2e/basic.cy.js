8a369e6a69274721a6d56c515702e213f4faa622
livinsmm <128262140+livinsmm@users.noreply.github.com>
2023-03-19 07:00:04 +0530

    Update basic.cy.js

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -4,6 +4,6 @@ describe('sample render test', () => {
   });
 
   it('displays the title text', () => {
-    cy.get('h1').contains('Virtual campus by Livin Company');
+    cy.get('h1').contains('IDVC by Livin Company');
   });
 });
