537468da8241180cdedfdcaaae750bed5e256106
livinsmm <128262140+livinsmm@users.noreply.github.com>
2023-03-19 06:49:05 +0530

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Indian interior designers virtual campus';
+    : 'Interior designers community';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Most vibrant designers community';
+    : 'Virtual campus by Livin Company';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved. @livincompany';
